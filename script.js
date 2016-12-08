ca490b4a1dd031c10fc06c9f2e2835cf541ad00d
simplonco <simplonco@debian>
2016-12-08 11:28:47 +0100

    debuggage overflow refresh

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 var boutonEnter = document.getElementById('bouton-enter');
 var header = document.getElementById('header-main');
-var bodyPage = document.getElementById('body')
+var bodyPage = document.getElementById('body');
 
+window.onbeforeunload = function () {
+  window.scrollTo(0, 0);
+}
 
 boutonEnter.onclick = function() {
     header.classList.add('toMainPage');
